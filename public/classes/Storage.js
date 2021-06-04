b19842c4e1e1df3c4719d1be08dc0d4ed6acc53b
Lebeil <liedel75@gmail.com>
2021-06-04 20:36:07 +0200

    add storage invoice & estimate

---
+++ public/classes/Storage.js
@@ -0,0 +1,10 @@
+export class Storage {
+    static checkLocalStorage() {
+        if (localStorage.getItem('invoice') === null) {
+            localStorage.setItem('invoice', '[]');
+        }
+        if (localStorage.getItem('estimate') === null) {
+            localStorage.setItem('estimate', '[]');
+        }
+    }
+}
